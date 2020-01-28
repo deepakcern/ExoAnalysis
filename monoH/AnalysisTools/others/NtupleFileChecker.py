3b481af92540f1880b2149a8a0192d3205474e2e
Deepak Kumar <dekumar@Deepaks-iMac.local>
2020-01-28 13:41:31 +0530

    updated NtupleChecker

--- monoH/AnalysisTools/others/NtupleFileChecker.py
+++ monoH/AnalysisTools/others/NtupleFileChecker.py
@@ -2,21 +2,24 @@ import os
 from ROOT import TFile,TChain
 from glob import glob
 
-path='/eos/cms/store/group/phys_exotica/bbMET/2017_skimmedFiles/V0/MC_USCM_25Sep'
+path='/home/deepakk/NewSkimmer/V1/ExoPieSlimmer/CondorJobs/FailedFiles_mc'
 
 files=glob(path+'/*.txt')
 
 deadfile=open('deadNtuplesFiles.txt','w')
 
+#print 'Files', files
 for infile in files:
-    for line in infile:
+    f = open(infile,'r')
+    for line in f:
+        #print 'fileName', line
         try:
-        Tree = TChain("tree/treeMaker")
-        Tree.Add(line)
-        NEntries = Tree.GetEntries()
-        if NEntries==0 or NEntries<0:
-            print 'File detected with zero entries'
-	    deadfile.write(infile+'\n')
+            Tree = TChain("tree/treeMaker")
+            Tree.Add(line.rstrip())
+            NEntries = Tree.GetEntries()
+            if NEntries==0 or NEntries<0:
+                print 'File detected with zero entries'
+	        deadfile.write(infile+'\n')
         except Exception as e:
 	    print e
             print "Corrupt file detected"
